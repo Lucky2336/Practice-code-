aaed4fa9981bc8c32d4b5d8723b64f5fc204a9ad
Lucky2336 <luckyyadav6697@gmail.com>
2025-11-26 10:39:59 +0530

    Add files via upload

---
+++ ooltapyramid.java
@@ -0,0 +1,20 @@
+public class ooltapyramid {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            System.out.println(" ");
+            for( int j=1;j<=i;j++){
+               
+                System.out.print(" ");
+            }
+             for( int k=5;k>=i;k--){
+                System.out.print("*");
+        }
+        for(int p=5;p>i;p--){
+            System.out.print("*");
+        
+        }
+
+    }
+}
+}
+
